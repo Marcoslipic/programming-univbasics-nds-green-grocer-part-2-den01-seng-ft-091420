d261983affc69af2bcccd4da668dc06fbdfcd011
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-06 07:00:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -55,6 +55,7 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidate_cart = consolidate_cart(car)
   
   
 end
