fdb61a8ff7e7995b30bc928f8eadd9e46cc156ea
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-06 07:00:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,12 +4,45 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  counter = 0 
+  while counter < coupons.length 
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+  couponed_items_name = "#{coupons[counter][:item]} W/COUPON"
+  cart_item_with_coupon = find_item_by_name_in_collection(couponed_items_name, cart)
+  if cart_item && cart_item[:count] >= coupons[counter][:num]
+    if cart_item_with_coupon
+      cart_item_with_coupon[:count] += coupons[counter][:num]
+      cart_item[:count] -= coupons[counter][:num]
+    else 
+      cart_item_with_coupon = {
+        :item => couponed_items_name,
+        :price => coupons[counter][:cost] / coupons[counter][:num],
+        :count => coupons[counter][:num],
+        :clearance => cart_item[:clearance]
+      }
+      cart << cart_item_with_coupon
+      cart_item[:count] -= coupons[counter][:num]
+    end 
+  end 
+  counter += 1 
+end 
+  return cart
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < cart.length 
+  if cart[counter][:clearance]
+    cart[counter][:price] *= 0.8
+    cart[counter][:price].round(2)
+  end 
+  counter += 1 
+end 
+  cart 
 end
 
 def checkout(cart, coupons)
@@ -22,4 +55,6 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  
 end
